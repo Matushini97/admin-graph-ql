9bccb397e02cb029264c7773d4c9262f969561aa
Ilya Matyushin <IVMatyushin@alfabank.ru>
2025-07-26 14:16:25 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
-export type FormType = { email: string; password: string }
+'use client'
 
-export default function Home() {
-  return <div>Users List</div>
+import { useGetUsersQuery } from '@/api/queries/user-list.generated'
+import { Loader, Table, TableHeader } from '@vibe-samurai/visual-ui-kit'
+import React from 'react'
+
+import { COLUMNS } from '@/constants/table-columns'
+import { TableActionButtons } from '@/components/table-action-buttons/table-action-buttons'
+import BlockIcon from '@public/icons/BlockIcon'
+
+export default function Users() {
+  const { data, loading } = useGetUsersQuery()
+
+  const usersList = data?.getUsers.users
+
+  if (loading) {
+    return <Loader />
+  }
+
+  return (
+    <Table.Root>
+      <TableHeader columns={COLUMNS} />
+      <Table.Body>
+        {usersList?.map(({ id, createdAt, profile, userName, userBan }) => (
+          <Table.Row key={id}>
+            <Table.Cell>
+              {userBan ? <BlockIcon /> : ''}
+              {id}
+            </Table.Cell>
+            <Table.Cell>{userName}</Table.Cell>
+            <Table.Cell>
+              {profile.firstName} {profile.lastName}
+            </Table.Cell>
+            <Table.Cell>{new Date(createdAt).toUTCString()}</Table.Cell>
+            <Table.Cell>
+              <TableActionButtons id={id} userBan={Boolean(userBan)} userName={userName} />
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
 }
